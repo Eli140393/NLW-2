4d8bc65f74936aece0f5f11a731e40ace32478e9
Eliezer <eli.defilicibus@gmail.com>
2021-05-25 10:57:25 -0300

    Projeto iniciado

---
+++ src/pages/TeacherForm/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+function TeacherForm(){
+    return(
+        <h1> TeacherForm</h1>
+    )
+}
+ export default TeacherForm;
---
+++ src/pages/TeacherList/index.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import backIcon from '../../assets/images/icons/back.svg';
+import logoImg from '../../assets/images/icons/back.svg';
+
+function TeacherList(){
+    return(
+
+        <div id="page-teacher-list" className="container">
+            <header className="page-header">
+            <div className="top-bar-container">
+                <Link to="/"/>
+               
+            </div>
+            </header>
+
+        </div>
+    )
+}
+ export default TeacherList;
