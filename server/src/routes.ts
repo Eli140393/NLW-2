79393a7d77919ce826bbd86351c0318cbc32fbbb
Eliezer <eli.defilicibus@gmail.com>
2021-06-18 08:35:52 -0300

    functions utills

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,12 +1,57 @@
 import express from 'express';
+import db from './database/connection';
+import convertHourToMinutes from './utills/convertHourToMinutes';
+
+
 const routes = express.Router();
 
+ 
+ interface ScheduleItem {
+     week_day: number,
+     from: string,
+     to: string
+ };
+
+routes.post('/classes', async (request, response) => {
+   const {
+       name,
+       avatar,
+       whatsapp,
+       bio,
+       subject,
+       cost,
+       schedule
+   } = request.body; 
+
+const insertedUsersIds = await db('users').insert({
+    name,
+    avatar,
+    whatsapp,
+    bio,
+});
+
+const user_id = insertedUsersIds[0];
 
 
+const insertedClassesIds = await db('classes').insert({
+subject,
+cost,
+user_id,
+});
 
+const class_id = insertedClassesIds[0];
 
-routes.get('/', (request, response) => {
-    return response.json({message: 'Hello World'}); 
+const classSchedule = schedule.map((scheduleItem: ScheduleItem) => {
+ return {
+    class_id,
+    week_day: scheduleItem.week_day,
+    from: convertHourToMinutes(scheduleItem.from),
+    to: convertHourToMinutes(scheduleItem.to),
+};
+})
+
+ await db('class_schedule').insert(classSchedule)
+   return response.send();
 });
 
 export default routes;
