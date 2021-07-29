0d441c3b2e2b66f1eeb1d28a5b371f70ae7469c9
Eliezer <eli.defilicibus@gmail.com>
2021-07-29 11:11:52 -0300

    create routes

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,9 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import Landing from './src/pages/Landing';
+
 import AppLoading  from 'expo-app-loading';
 import {Archivo_400Regular, Archivo_700Bold, useFonts} from '@expo-google-fonts/archivo'
 import {Poppins_400Regular, Poppins_600SemiBold} from '@expo-google-fonts/poppins'
+import AppStack from './src/routes/AppStack';
 
 
 
@@ -21,7 +22,8 @@ export default function App() {
 
   return (
     <>
-    <Landing />
+    <AppStack />
+
      <StatusBar style="light" />
      </>
   );
