26c0653874381cda0de9ebfee0b785d8a75a771e
Eliezer <eli.defilicibus@gmail.com>
2021-08-08 09:05:48 -0300

    Create give classes

--- mobile/src/pages/GiveClasses/index.tsx
+++ mobile/src/pages/GiveClasses/index.tsx
@@ -1,8 +1,25 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, ImageBackground, Text } from 'react-native';
+
+import giverClassesBgImage from '../../assets/images/give-classes-background.png';
+import styles from './styles';
+
 
 function GiveClasses(){
-    return <View/>
+    return (
+    <View style={styles.container}>
+        <ImageBackground
+          resizeMode="contain"
+           source={giverClassesBgImage}
+            style={styles.content}
+            >
+               <Text style={styles.title}> Quer ser um Proffy?</Text> 
+                <Text style={styles.description}>
+                Para começar, você precisa se cadastrar como professor na plataforma web
+                </Text>
+            </ImageBackground>
+    </View>
+    );
 }
 
 export default GiveClasses;
